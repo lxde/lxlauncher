e7208e39407fcd31e3e55d1beb029bcf504f7ffa
Andrew Lee <ajqlee@debian.org>
2009-01-09 07:02:09 +0000

    Brazilian Portuguese translation for LXLauncher. (Closes:#2490039)

--- src/lxlauncher.c
+++ src/lxlauncher.c
@@ -662,7 +662,7 @@ int main(int argc, char** argv)
     // Add application specific properties
     gtk_settings_install_property(g_param_spec_boolean("lxlauncher-enable-key",
                             _("Enable key navigation"),
-                            _("Allow users to use up/down/left/right/tabe/enter keys to operate the lxlaucher"),
+                            _("Allow users to use up/down/left/right/tab/enter keys to operate the lxlaucher"),
                             FALSE,GTK_ARG_READWRITE));
     // set up themes for notebook
     gtk_rc_parse( PACKAGE_DATA_DIR "/lxlauncher/gtkrc" );
